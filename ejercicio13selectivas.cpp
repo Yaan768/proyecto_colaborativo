059c754cab5ff8170f80f5b016e1ae430b9f806b
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-03 22:09:42 -0500

    Actualizacion ejercicio13selectivas : agregacion de validaciones

--- ejercicio13selectivas.cpp
+++ ejercicio13selectivas.cpp
@@ -1,14 +1,22 @@
 #include <iostream>//formar un triangulo
 using namespace std;
+
 int main() {
     float a, b, c;//a,b,c: longitudes a ingresar 
+    
     cout << "Ingrese las tres longitudes: ";
     cin >> a >> b >> c;
-    //Proceso
-    if (a + b > c and a + c > b and b + c > a) {
+    
+    if (a<=0 || b<=0 || c<0){
+    	cout << "Las longitudes deben ser mayores que cero" << endl;
+    	return 1;
+	}
+	
+    if (a + b > c && a + c > b && b + c > a) {
         cout << "Se puede formar un triangulo." << endl;//Salida
     } 
 	else {
         cout << "No se puede formar un triangulo." << endl;//salida
     }
+    return 0;
 }
