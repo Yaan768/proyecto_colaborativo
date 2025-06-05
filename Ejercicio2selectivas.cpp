eda9b19b1e6a4cef4232322a54d096b30f846cd5
Yaan768 <yaan7682019@gmail.com>
2025-06-04 23:26:57 -0500

    Actualizacion del ejercicio 2 de selectivas: Se agrego validacion

--- Ejercicio2selectivas.cpp
+++ Ejercicio2selectivas.cpp
@@ -3,11 +3,22 @@ using namespace std;
 
 int main() {
     float soles, tipoC, dolares;
+    
+    //ENTRADA
     cout << "Ingrese la cantidad en soles: ";
     cin >> soles;
+
     cout << "Ingrese el tipo de cambio: ";
     cin >> tipoC;
-    dolares = soles / tipoC;
-    cout << "La cantidad en dolares es: $" << dolares << endl;
+
+    // VALIDACION Y PROCESO
+    if (tipoC <= 0) {
+        cout << "Error: El tipo de cambio debe ser mayor que cero." << endl;//SALIDA
+    } else {
+        dolares = soles / tipoC;
+        cout << "La cantidad en dolares es: $" << dolares << endl;//SALIDA
+    }
+
     return 0;
 }
+
