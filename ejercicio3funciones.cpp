d19acfbfe3cf4403931458622104d3e2b73bdd6a
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-07 23:42:18 -0500

    Actualizacion del ejercicio 3 funciones : Mejorar verificador de números primos con interfaz en consola

--- ejercicio3funciones.cpp
+++ ejercicio3funciones.cpp
@@ -4,7 +4,13 @@ using namespace std;
 // Función que determina si un número es primo
 bool esPrimo(int num) {
     if (num <= 1) {
-        return false; // Los números menores o iguales a 1 no son primos
+        return false; 
+    }
+    if (num ==2) {
+        return true; 
+    }
+    if (num % 2 == 0) {
+        return false; 
     }
 
     for (int i = 2; i * i <= num; ++i) { 
@@ -17,15 +23,32 @@ bool esPrimo(int num) {
 
 int main() {
     int numero;
+    cout << "============================" << endl;
+    cout << "VERFICADOR DE NUMEROS PRIMOS" << endl;
+    cout << "============================" << endl;
+    cout << "Este programa verifica si un numero es primo." << endl;
     
-    cout << "Introduce un número: ";
+    
+    cout << "Ingresa un numero entero : ";
     cin >> numero;
+
+    if (!cin) {
+        cout << "Entrada no válida. Por favor, ingresa un número entero." << endl;
+        return 1;
+    } else {
+        cout << "\nProcesando el número " << numero << "..." << endl;
+    }
 
     if (esPrimo(numero)) {
         cout << "El número " << numero << " es primo." << endl;
     } else {
         cout << "El número " << numero << " no es primo." << endl;
     }
+
+    cout << "\nGracias por usar el verificador de numeros primos." << endl;
+    cout << "============================" << endl;
+    cout << "Fin del programa." << endl;
+    cout << "============================" << endl;
 
     return 0;
 }
